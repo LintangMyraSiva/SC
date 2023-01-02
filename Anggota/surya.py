bf04379126f656ce0d7dbaa42de5892121bd00c1
SuryaAryaPutra <119647962+SuryaAryaPutra@users.noreply.github.com>
2023-01-02 23:09:42 +0700

    Update surya.py

--- Anggota/surya.py
+++ Anggota/surya.py
@@ -1,2 +1,68 @@
-x = "farhan sama cipa"
-print(x)
+import pyaudio
+import wave
+import matplotlib.pyplot as mpl
+import numpy as npy
+
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+
+pa = pyaudio.PyAudio()
+
+stream = pa.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=FRAMES_PER_BUFFER
+)
+
+print('start recording')
+
+seconds = 8
+frames = []
+second_tracking = 0
+second_count = 0
+for i in range(0, int(RATE/FRAMES_PER_BUFFER*seconds)):
+    data = stream.read(FRAMES_PER_BUFFER)
+    frames.append(data)
+    second_tracking += 1
+    if second_tracking == RATE/FRAMES_PER_BUFFER:
+        second_count += 1
+        second_tracking = 0
+        print(f'Time Left: {seconds - second_count} seconds')
+
+
+stream.stop_stream()
+stream.close()
+pa.terminate()
+
+obj = wave.open('lemaster_tech.wav', 'wb')
+obj.setnchannels(CHANNELS)
+obj.setsampwidth(pa.get_sample_size(FORMAT))
+obj.setframerate(RATE)
+obj.writeframes(b''.join(frames))
+obj.close()
+
+file = wave.open('lemaster_tech.wav', 'rb')
+
+sample_freq = file.getframerate()
+frames = file.getnframes()
+signal_wave = file.readframes(-1)
+
+file.close()
+
+time = frames / sample_freq
+
+audio_array = npy.frombuffer(signal_wave, dtype=npy.int16)
+
+times = npy.linspace(0, time, num=frames)
+
+mpl.figure(figsize=(15, 5))
+mpl.plot(times, audio_array)
+mpl.ylabel('Signal Wave')
+mpl.xlabel('Time (s)')
+mpl.xlim(0, time)
+mpl.title('The Thing I Just Recorded!!')
+mpl.show()
